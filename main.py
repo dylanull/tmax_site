937b78e30d6c3dda40eab772360d6b469e549275
Dylan Longwith <96076659+dylanull@users.noreply.github.com>
2024-06-24 23:06:40 -0400

    all pages now have functioning views

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
-import pandas
+from flask_sqlalchemy import SQLAlchemy
+import pandas, sqlite3
 
 app = Flask(__name__)
 
@@ -13,18 +14,32 @@ def select_file():
     return render_template('select-file.html')
 
 @app.route('/tmx')
+def search_tmx():
+    return render_template('tmx.html')
+
+@app.route('/src')
+def search_src():
+    return render_template('src.html')
+
+@app.route('/cic')
+def search_cic():
+    return render_template('cic.html')
+
+
+#### Spreadsheet Views
+@app.route('/tmx_spreadsheet')
 def tmx_spreadsheet():
     file = "source/tmax.xlsx"
     data = pandas.read_excel(file)
     return data.to_html()
 
-@app.route('/src')
+@app.route('/src_spreadsheet')
 def src_spreadsheet():
     file = "source/src.xlsx"
     data = pandas.read_excel(file)
     return data.to_html()
 
-@app.route('/cic')
+@app.route('/cic_spreadsheet')
 def cic_spreadsheet():
     file = "source/cic.xlsx"
     data = pandas.read_excel(file)
